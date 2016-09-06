435ea9094f74446a0deb5d08ddfa205eb9df7ef5
TheGithubNub <tangd46@gmail.com>
2016-09-05 17:39:11 -0700

    Test

--- src/Chat.java
+++ src/Chat.java
@@ -1,4 +1,50 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Chat {
+import javax.swing.*;
+import java.net.*;
+public class Chat implements ActionListener{
+	JLabel label;
+	JFrame frame;
+	JTextField textField;
+	Chat(){
+		textField = new JTextField();
+		frame = new JFrame("SOME SIMPLE CHAT CLIENT YOU SHOULD'NT USE");
+		frame.setPreferredSize(new Dimension(1000,1000));
+		frame.setLayout(new BorderLayout());
+		frame.setVisible(true);
+		textField.setPreferredSize(new Dimension(100,60));
+		frame.add(textField, BorderLayout.SOUTH);
+		frame.pack();
+		textField.addKeyListener(new KeyListener(){
 
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode() == KeyEvent.VK_ENTER && !(textField.getText().trim() == "")){
+					System.out.println("HI");
+					System.out.println(textField.getText());
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+	}
+	public void actionPerformed(ActionEvent ev) {
+		System.out.println("HI");
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		Chat chat = new Chat();
 
 	}
 
